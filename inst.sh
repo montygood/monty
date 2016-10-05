281bba39d68d1e2e6e12bbe5a006bf3d283f58a1
montygood <p.birbaumer@gmx.ch>
2016-10-05 10:43:13 +0200

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 op_title=" -| Arch Linux |- "
+log="error.log"
 arch_chroot() {
-	arch-chroot /mnt /bin/bash -c "${1}"
+    arch-chroot /mnt /bin/bash -c "${1}"
 }  
 pac_strap() {
-	pacstrap /mnt ${1} --needed
+    pacstrap /mnt ${1} --needed 2>> $log
 }  
 load() {
 	{	int="1"
         	while ps | grep "$pid" &> /dev/null
     	    	do
-    	        	sleep $pri
-    	        	echo $int
+    	            sleep $pri
+    	            echo $int
     	        	if [ "$int" -lt "100" ]; then
     	        		int=$((int+1))
     	        	fi
@@ -43,18 +44,18 @@ init() {
 	clear
 	pacman -Syy
 	#mac
-	if [[ "$(cat /sys/class/dmi/id/sys_vendor)" == 'Apple Inc.' ]] || [[ "$(cat /sys/class/dmi/id/sys_vendor)" == 'Apple Computer, Inc.' ]]; then
+    if [[ "$(cat /sys/class/dmi/id/sys_vendor)" == 'Apple Inc.' ]] || [[ "$(cat /sys/class/dmi/id/sys_vendor)" == 'Apple Computer, Inc.' ]]; then
 		modprobe -r -q efivars || true  # if MAC
-	else
+    else
 		modprobe -q efivarfs
-	fi
+    fi
 	#efi
-	if [[ -d "/sys/firmware/efi/" ]]; then
+    if [[ -d "/sys/firmware/efi/" ]]; then
 		if [[ -z $(mount | grep /sys/firmware/efi/efivars) ]]; then mount -t efivarfs efivarfs /sys/firmware/efi/efivars ; fi
 		SYSTEM="UEFI"
-	else
+    else
 		SYSTEM="BIOS"
-	fi
+    fi
 	_keys
 }
 _keys() {
@@ -71,7 +72,7 @@ _rpass() {
 	RPASSWD1=$(dialog --backtitle "$op_title" --title " -| Root |- " --stdout --clear --insecure --passwordbox "Passwort:" 0 0 "")
 	RPASSWD2=$(dialog --backtitle "$op_title" --title " -| Root |- " --stdout --clear --insecure --passwordbox "Passwort bestätigen:" 0 0 "")
 	if [[ $RPASSWD1 == $RPASSWD2 ]]; then 
-		echo -e "${RPASSWD1}\n${RPASSWD1}" > /tmp/.rpasswd
+	   echo -e "${RPASSWD1}\n${RPASSWD1}" > /tmp/.rpasswd 2>> $log
 	else
 		dialog --backtitle "$op_title" --title " -| FEHLER |- " --infobox "\nDie eingegebenen Passwörter stimmen nicht überein." 0 0
 		_rpass
@@ -80,24 +81,24 @@ _rpass() {
 }
 _nuser() {
 	USER=$(dialog --backtitle "$op_title" --title " -| Benutzer |- " --stdout --inputbox "Namen des Benutzers in Kleinbuchstaben." 0 0 "")
-	if [[ ${#USER} -eq 0 ]] || [[ $USER =~ \ |\' ]] || [[ $USER =~ [^a-z0-9\ ] ]]; then
+	if [[ ${$USER} -eq 0 ]] || [[ $USER =~ \ |\' ]] || [[ $USER =~ [^a-z0-9\ ] ]]; then
 		dialog --backtitle "$op_title" --title " -| FEHLER |- " --msgbox "Ungültiger Benutzername." 0 0
 		_nuser
 	fi
 	_puser
 }
-_puser () {
+_puser () { #benutzer passwort
 	PASSWD1=$(dialog --backtitle "$op_title" --title " -| Benutzer $USER |- " --stdout --clear --insecure --passwordbox "Passwort" 0 0 "")
 	PASSWD2=$(dialog --backtitle "$op_title" --title " -| Benutzer $USER |- " --stdout --clear --insecure --passwordbox "Passwort bestätigen" 0 0 "")
 	if [[ $PASSWD1 == $PASSWD2 ]]; then
-		echo -e "${PASSWD1}\n${PASSWD1}" > /tmp/.passwd
+		echo -e "${PASSWD1}\n${PASSWD1}" > /tmp/.passwd 2>> $log
 	else
 		dialog --backtitle "$op_title" --title " -| FEHLER |- " --msgbox "\nDie eingegebenen Passwörter stimmen nicht überein." 0 0
 		_puser
 	fi
 	_inst
 }
-_inst() {
+_inst() { #Fragen
 	op_title=" -| Gebietsschema einstellen |- "
 	LOCALE=$(dialog --backtitle "$op_title" --nocancel --menu "Wählen Sie Ihr Gebietsschema aus:" 10 35 11 "de_CH.UTF-8" "Schweiz" "$other""Auswahl" 3>&1 1>&2 2>&3)
 	if [ "$LOCALE" = "$other" ]; then
@@ -153,21 +154,21 @@ _inst() {
 	op_title=" -| Arch Linux - ($(uname -m)) $SYSTEM $HD_SD |- "
 	_mirrors
 }
-_mirrors() {
+_mirrors() { #mirror
 	if ! (</etc/pacman.d/mirrorlist2 grep "rankmirrors" &>/dev/null) then
 		(wget --no-check-certificate --append-output=/dev/null "https://www.archlinux.org/mirrorlist/?country=$code&protocol=http" -O /etc/pacman.d/mirrorlist.bak
 		echo "$?" > /tmp/ex_status.var ; sleep 0.5) &> /dev/null & pid=$! pri=0.1 msg="Eine neue Spiegelserver-Liste wird abgerufen..." load
-		sed -i 's/#//' /etc/pacman.d/mirrorlist.bak
-		rankmirrors -n 6 /etc/pacman.d/mirrorlist.bak > /etc/pacman.d/mirrorlist & pid=$! pri=0.8 msg="Bitte warten. Spiegelserver werden nach Schnelligkeit sortiert..." load
+		sed -i 's/#//' /etc/pacman.d/mirrorlist.bak 2>> $log
+		rankmirrors -n 6 /etc/pacman.d/mirrorlist.bak > /etc/pacman.d/mirrorlist 2>> $log & pid=$! pri=0.8 msg="Bitte warten. Spiegelserver werden nach Schnelligkeit sortiert..." load
 		chmod +r /etc/pacman.d/mirrorlist
 		pacman-key --init
-		pacman-key --populate archlinux >/dev/null
-		pacman-key --refresh-keys 2>.err & pid=$! pri=0.8 msg="Spiegelserver-Schlüssel werden geupdatet..." load
+		pacman-key --populate archlinux >/dev/null 2>> $log
+		pacman-key --refresh-keys  2>> $log & pid=$! pri=0.8 msg="Spiegelserver-Schlüssel werden geupdatet..." load
 		pacman -Syy
 	fi
 	_part
 }
-_part() {
+_part() { #partitionen
 	_umount
 	if [[ $WIPE == "YES" ]]; then
 		clear
@@ -179,17 +180,17 @@ _part() {
     fi
     if [[ $SYSTEM == "BIOS" ]]; then
 		sgdisk --zap-all /dev/${IDEV}
-		echo -e "o\ny\nn\n1\n\n+1M\nEF02\nn\n2\n\n\n\nw\ny" | gdisk /dev/${IDEV}
+		echo -e "o\ny\nn\n1\n\n+1M\nEF02\nn\n2\n\n\n\nw\ny" | gdisk /dev/${IDEV} 2>> $log
 		dialog --backtitle "$btitle" --title " -| Bereite BIOS Festplatte vor |- " --infobox "\n...Bitte warten..." 0 0
-		echo j | mkfs.ext4 -L arch /dev/${IDEV}2 >/dev/null
+		echo j | mkfs.ext4 -L arch /dev/${IDEV}2 >/dev/null 2>> $log
 		mount /dev/${IDEV}2 /mnt
 	fi
 	if [[ $SYSTEM == "UEFI" ]]; then
 		sgdisk --zap-all /dev/${IDEV}
-		echo -e "n\n\n\n512M\nef00\nn\n\n\n\n\nw\ny" | gdisk /dev/${IDEV}
+		echo -e "n\n\n\n512M\nef00\nn\n\n\n\n\nw\ny" | gdisk /dev/${IDEV} 2>> $log
 		dialog --backtitle "$btitle" --title " -| Bereite UEFI Festplatte vor |- " --infobox "\n...Bitte warten..." 0 0
-		echo j | mkfs.vfat -F32 -L boot /dev/${IDEV}1 >/dev/null
-		echo j | mkfs.ext4 -L arch /dev/${IDEV}2 >/dev/null
+		echo j | mkfs.vfat -F32 -L boot /dev/${IDEV}1 >/dev/null 2>> $log
+		echo j | mkfs.ext4 -L arch /dev/${IDEV}2 >/dev/null 2>> $log
 		mount /dev/${IDEV}2 /mnt
 		mkdir -p /mnt/boot
 		mount /dev/${IDEV}1 /mnt/boot
@@ -204,7 +205,7 @@ _part() {
 	fi
 	_base
 }
-_base() {
+_base() { #Base
 	pac_strap "base base-devel"
 	if [[ $SYSTEM == "BIOS" ]]; then		
 		pac_strap "grub dosfstools"
@@ -218,32 +219,32 @@ _base() {
 		arch_chroot "grub-install --efi-directory=/boot --target=x86_64-efi --bootloader-id=arch_grub --recheck"
 		arch_chroot "grub-mkconfig -o /boot/grub/grub.cfg"
 	fi
-	sed -i "s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/" /mnt/etc/default/grub
-	sed -i "s/timeout=5/timeout=0/" /mnt/boot/grub/grub.cfg
-	arch_chroot "grep -q "timeout=0" /boot/grub/grub.cfg || grub-mkconfig"
-	echo "KEYMAP=${ILANG}" > /mnt/etc/vconsole.conf
+	sed -i "s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/" /mnt/etc/default/grub 2>> $log
+	sed -i "s/timeout=5/timeout=0/" /mnt/boot/grub/grub.cfg 2>> $log
+	arch_chroot "grep -q 'timeout=0' /boot/grub/grub.cfg || grub-mkconfig"
+	echo "KEYMAP=${ILANG}" > /mnt/etc/vconsole.conf 2>> $log
 	cp -f /etc/pacman.conf /mnt/etc/pacman.conf
 	if [ $(uname -m) == x86_64 ]; then
 		sed -i '/\[multilib]$/ {
 		N
-		/Include/s/#//g}' /mnt/etc/pacman.conf
+		/Include/s/#//g}' /mnt/etc/pacman.conf 2>> $log
 	fi
 	if ! (</mnt/etc/pacman.conf grep "archlinuxfr"); then
-		echo -e "\n[archlinuxfr]\nServer = http://repo.archlinux.fr/$(uname -m)\nSigLevel = Never" >> /mnt/etc/pacman.conf
+		echo -e "\n[archlinuxfr]\nServer = http://repo.archlinux.fr/$(uname -m)\nSigLevel = Never" >> /mnt/etc/pacman.conf 2>> $log
 	fi
 	arch_chroot "pacman -Syy"
 	_sets
 }
-_sets() {
+_sets() { #Einstellungen
 	#genf
-	genfstab -U -p /mnt >> /mnt/etc/fstab
-	[[ -f /mnt/swapfile ]] && sed -i "s/\\/mnt//" /mnt/etc/fstab
+	genfstab -U -p /mnt >> /mnt/etc/fstab 2>> $log
+	[[ -f /mnt/swapfile ]] && sed -i "s/\\/mnt//" /mnt/etc/fstab 2>> $log
 	#hostname
-	echo "${HOSTN}" > /mnt/etc/hostname
-	echo -e "#<ip-address>\t<hostname.domain.org>\t<hostname>\n127.0.0.1\tlocalhost.localdomain\tlocalhost\t${HOSTN}\n::1\tlocalhost.localdomain\tlocalhost\t${HOSTN}" > /mnt/etc/hosts
+	echo "${HOSTN}" > /mnt/etc/hostname 2>> $log
+	echo -e "#<ip-address>\t<hostname.domain.org>\t<hostname>\n127.0.0.1\tlocalhost.localdomain\tlocalhost\t${HOSTN}\n::1\tlocalhost.localdomain\tlocalhost\t${HOSTN}" > /mnt/etc/hosts 2>> $log
 	#loc
-	echo "LANG=\"${LOCALE}\"" > /mnt/etc/locale.conf
-	sed -i "s/#${LOCALE}/${LOCALE}/" /mnt/etc/locale.gen
+	echo "LANG=\'${LOCALE}\'" > /mnt/etc/locale.conf 2>> $log
+	sed -i "s/#${LOCALE}/${LOCALE}/" /mnt/etc/locale.gen 2>> $log
 	arch_chroot "locale-gen" >/dev/null
 	#zon
 	arch_chroot "ln -s /usr/share/zoneinfo/${ZONE}/${SUBZONE} /etc/localtime"
@@ -256,9 +257,9 @@ _sets() {
 	arch_chroot "useradd ${USER} -m -g users -G wheel,storage,power,network,video,audio,lp -s /bin/bash"
 	arch_chroot "passwd ${USER}" < /tmp/.passwd
 	rm /tmp/.passwd
-	arch_chroot "cp /etc/skel/.bashrc /home/${USER}"
-	arch_chroot "chown -R ${USER}:users /home/${USER}"
-	[[ -e /mnt/etc/sudoers ]] && sed -i '/%wheel ALL=(ALL) ALL/s/^#//' /mnt/etc/sudoers
+	arch_chroot "cp /etc/skel/.bashrc /home/${USER}" 2>> $log
+	arch_chroot "chown -R ${USER}:users /home/${USER}" 2>> $log
+	[[ -e /mnt/etc/sudoers ]] && sed -i '/%wheel ALL=(ALL) ALL/s/^#//' /mnt/etc/sudoers 2>> $log
 	#mkinit
 	clear
 	KERNEL=""
@@ -268,7 +269,7 @@ _sets() {
 	done
 	_xorg
 }
-_xorg() {
+_xorg() { #XORG
 	pac_strap "xorg-server xorg-server-utils xorg-xinit"
 	pac_strap "xf86-input-synaptics xf86-input-mouse xf86-input-keyboard xf86-input-libinput"
 	user_list=$(ls /mnt/home/ | sed "s/lost+found//")
@@ -281,7 +282,7 @@ _xorg() {
 _graphics_card() {
 	install_intel(){
 		pacstrap /mnt xf86-video-intel libva-intel-driver intel-ucode
-		sed -i 's/MODULES=""/MODULES="i915"/' /mnt/etc/mkinitcpio.conf         
+		sed -i 's/MODULES=""/MODULES="i915"/' /mnt/etc/mkinitcpio.conf 2>> $log
 		if [[ -e /mnt/boot/grub/grub.cfg ]]; then
 			arch_chroot "grub-mkconfig -o /boot/grub/grub.cfg"
 		fi
@@ -295,7 +296,7 @@ _graphics_card() {
 	}
 	install_ati(){
 		pacstrap /mnt xf86-video-ati
-		sed -i 's/MODULES=""/MODULES="radeon"/' /mnt/etc/mkinitcpio.conf
+		sed -i 's/MODULES=""/MODULES="radeon"/' /mnt/etc/mkinitcpio.conf 2>> $log
 	}
 	NVIDIA=""
 	VB_MOD=""
@@ -326,7 +327,7 @@ _graphics_card() {
 	if [[ $HIGHLIGHT_SUB_GC == 3 ]] ; then
 		[[ $INTEGRATED_GC == "ATI" ]] &&  install_ati || install_intel
 		pacstrap /mnt xf86-video-nouveau
-		sed -i 's/MODULES=""/MODULES="nouveau"/' /mnt/etc/mkinitcpio.conf       
+		sed -i 's/MODULES=""/MODULES="nouveau"/' /mnt/etc/mkinitcpio.conf 2>> $log
 	fi
 	if [[ $HIGHLIGHT_SUB_GC == 4 ]] ; then
 		[[ $INTEGRATED_GC == "ATI" ]] &&  install_ati || install_intel
@@ -364,7 +365,7 @@ _graphics_card() {
 		umount -l /mnt/dev
 		arch_chroot "modprobe -a vboxguest vboxsf vboxvideo"  
 		arch_chroot "systemctl enable vboxservice"
-		echo -e "vboxguest\nvboxsf\nvboxvideo" > /mnt/etc/modules-load.d/virtualbox.conf
+		echo -e "vboxguest\nvboxsf\nvboxvideo" > /mnt/etc/modules-load.d/virtualbox.conf 2>> $log
 	fi
 	if [[ $HIGHLIGHT_SUB_GC == 9 ]] ; then
 		pacstrap /mnt xf86-video-vmware xf86-input-vmmouse
@@ -391,14 +392,14 @@ _desktopde() {
 	pac_strap "bash-completion gamin gksu python2-xdg ntfs-3g ttf-dejavu xdg-user-dirs xdg-utils poppler polkit"
 
 	pac_strap "lightdm lightdm-gtk-greeter"
-	arch_chroot "systemctl enable lightdm"
+    arch_chroot "systemctl enable lightdm"
 
-	sed -i 's/#autologin-user=/autologin-user=$USER/' /mnt/etc/lightdm/lightdm.conf
-	sed -i 's/#autologin-user-timeout=0/autologin-user-timeout=0/' /mnt/etc/lightdm/lightdm.conf
+	sed -i "s/#autologin-user=/autologin-user=${USER}/" /mnt/etc/lightdm/lightdm.conf 2>> $log
+	sed -i "s/#autologin-user-timeout=0/autologin-user-timeout=0/" /mnt/etc/lightdm/lightdm.conf 2>> $log
 	arch_chroot "groupadd -r autologin"
 	arch_chroot "gpasswd -a ${USER} autologin"
 
-	echo -e "Section "\"InputClass"\"\nIdentifier "\"system-keyboard"\"\nMatchIsKeyboard "\"on"\"\nOption "\"XkbLayout"\" "\"$keyde"\"\nEndSection" > /mnt/etc/X11/xorg.conf.d/00-keyboard.conf
+	echo -e "Section "\"InputClass"\"\nIdentifier "\"system-keyboard"\"\nMatchIsKeyboard "\"on"\"\nOption "\"XkbLayout"\" "\"$keyde"\"\nEndSection" > /mnt/etc/X11/xorg.conf.d/00-keyboard.conf 2>> $log
 	_hardware
 }
 _hardware() {
@@ -420,10 +421,10 @@ _hardware() {
 		arch_chroot "systemctl enable bluetooth.service"
 	fi
 	#Disable logs
-	sed -i "s/#Storage.*\|Storage.*/Storage=none/g" /mnt/etc/systemd/journald.conf
-	sed -i "s/SystemMaxUse.*/#&/g" /mnt/etc/systemd/journald.conf
-	sed -i "s/#Storage.*\|Storage.*/Storage=none/g" /mnt/etc/systemd/coredump.conf
-	echo "kernel.dmesg_restrict = 1" > /mnt/etc/sysctl.d/50-dmesg-restrict.conf
+	sed -i "s/#Storage.*\|Storage.*/Storage=none/g" /mnt/etc/systemd/journald.conf 2>> $log
+	sed -i "s/SystemMaxUse.*/#&/g" /mnt/etc/systemd/journald.conf 2>> $log
+	sed -i "s/#Storage.*\|Storage.*/Storage=none/g" /mnt/etc/systemd/coredump.conf 2>> $log
+	echo "kernel.dmesg_restrict = 1" > /mnt/etc/sysctl.d/50-dmesg-restrict.conf 2>> $log
 	_jdownloader
 }
 _jdownloader() {
@@ -432,7 +433,7 @@ _jdownloader() {
 	wget -c -O /mnt/opt/JDownloader/JDownloader.jar http://installer.jdownloader.org/JDownloader.jar
 	arch_chroot "chown -R 1000:1000 /opt/JDownloader/"
 	arch_chroot "chmod -R 0775 /opt/JDownloader/"
-	echo "[Desktop Entry]" >> /mnt/usr/share/applications/JDownloader.desktop
+	echo "[Desktop Entry]" >> /mnt/usr/share/applications/JDownloader.desktop 2>> $log
 	echo "Name=JDownloader" >> /mnt/usr/share/applications/JDownloader.desktop
 	echo "Comment=JDownloader" >> /mnt/usr/share/applications/JDownloader.desktop
 	echo "Exec=java -jar /opt/JDownloader/JDownloader.jar" >> /mnt/usr/share/applications/JDownloader.desktop
@@ -458,7 +459,7 @@ _appsinst() {
 _mediaelch() {
 	if [[ $MPC == "YES" ]]; then		
 		arch_chroot "yaourt -S mediaelch --noconfirm --needed"
-		echo "#!/bin/sh" >> /mnt/usr/bin/elch
+		echo "#!/bin/sh" >> /mnt/usr/bin/elch 2>> $log
 		echo "wakeonlan 00:01:2e:3a:5e:81" >> /mnt/usr/bin/elch
 		echo "sudo mkdir /mnt/Serien1" >> /mnt/usr/bin/elch
 		echo "sudo mkdir /mnt/Serien2" >> /mnt/usr/bin/elch
@@ -482,7 +483,7 @@ _mediaelch() {
 		echo "sudo rmdir /mnt/Filme2" >> /mnt/usr/bin/elch
 		echo "sudo rmdir /mnt/Musik" >> /mnt/usr/bin/elch
 		mkdir -p /mnt/home/${USER}/.config/kvibes/
-		echo "[Directories]" >> /mnt/home/${USER}/.config/kvibes/MediaElch.conf
+		echo "[Directories]" >> /mnt/home/${USER}/.config/kvibes/MediaElch.conf 2>> $log
 		echo "Concerts\size=0" >> /mnt/home/${USER}/.config/kvibes/MediaElch.conf
 		echo "Downloads\1\autoReload=false" >> /mnt/home/${USER}/.config/kvibes/MediaElch.conf
 		echo "Downloads\1\path=/home/monty/Downloads" >> /mnt/home/${USER}/.config/kvibes/MediaElch.conf
@@ -548,8 +549,9 @@ _yaourtinst() {
 }
 _MENU() {
 	_umount
+	nano $log
+#	dialog --backtitle "$btitle" --title " -| Installation Fertig |- " --msgbox "Install Medium entfernen" 0 0
 	reboot
-	dialog --backtitle "$btitle" --title " -| Installation Fertig |- " --msgbox "Install Medium entfernen" 0 0
 	exit 0
 }
 	#"b43-fwcutter" "Broadcom 802.11b/g/n" off \
