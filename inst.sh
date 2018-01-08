9ed52238ca7e5e19bc5d6b1094872aa10bd21230
montygood <p.birbaumer@gmx.ch>
2018-01-08 16:23:58 +0100

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -8,7 +8,7 @@ esac
 LOCALE="de_CH.UTF-8"
 LANGUAGE="de_DE"
 KEYMAP="de_CH-latin1"
-ZONE="Europe"
+ZONE="Europe"kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk,
 SUBZONE="Zurich"
 XKBMAP="ch"
 export LANG=${LOCALE}
@@ -398,7 +398,7 @@ set_mediaelch() {
 	mv aic94xx-seq.fw /mnt/lib/firmware/
 	mv wd719x-risc.bin /mnt/lib/firmware/
 	mv wd719x-wcs.bin /mnt/lib/firmware/
-	arch_chroot "mkinitcpio -p linux-lts"
+	arch_chroot "mkinitcpio -p linux"
 	#xorg
 	arch_strap "bc rsync mlocate pkgstats ntp bash-completion mesa gamin gksu gnome-keyring gvfs gvfs-mtp ifuse gvfs-afc gvfs-gphoto2 gvfs-nfs gvfs-smb polkit poppler python2-xdg ntfs-3g f2fs-tools fuse fuse-exfat mtpfs ttf-dejavu xdg-user-dirs xdg-utils autofs unrar p7zip lzop cpio zip arj unace unzip"
 	arch_strap "xorg-server xorg-apps xorg-xinit xorg-xkill xorg-twm xorg-xclock xterm xf86-input-keyboard xf86-input-mouse xf86-input-libinput"
