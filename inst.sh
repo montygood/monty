a7bb78caa02818bff85d62b3d13ee5b5a759a677
montygood <p.birbaumer@gmx.ch>
2019-01-09 00:08:11 +0100

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -310,7 +310,7 @@ EOF
 	[[ $(dmesg | egrep Tablet) != "" ]] && pacstrap /mnt xf86-input-wacom --needed --noconfirm
 	[[ $HD_SD == "SSD" ]] && arch_chroot "systemctl enable fstrim && systemctl enable fstrim.timer"
 	[[ ${ARCHI} == x86_64 ]] && arch_chroot "pacman -S lib32-alsa-plugins lib32-libpulse --needed --noconfirm"
-	if [[ $(lsusb | grep Fingerprint) != "" ]]; then		
+	if [[ $(lsusb | grep 1Fingerprint) != "" ]]; then		
 		arch_chroot "su - ${USERNAME} -c 'trizen -S fingerprint-gui --noconfirm'"
 		arch_chroot "usermod -a -G plugdev,scanner ${USERNAME}"
 		if ! (</mnt/etc/pam.d/sudo grep "pam_fingerprint-gui.so"); then sed -i '2 i\auth\t\tsufficient\tpam_fingerprint-gui.so' /mnt/etc/pam.d/sudo ; fi
