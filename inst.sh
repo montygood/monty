bb5026621f3706c9f2abfdebac14844a2c17d16c
montygood <p.birbaumer@gmx.ch>
2019-01-18 23:17:00 +0100

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -373,11 +373,12 @@ EOF
 	arch-chroot /mnt /bin/bash -c "chmod +x /bin/myup"
 	#update
 	mv monty.tar.xz /mnt/ && arch-chroot /mnt /bin/bash -c "tar xvf monty.tar.xz" && rm /mnt/monty.tar.xz
+	arch-chroot /mnt /bin/bash -c "echo $RPASSWD | su - ${USERNAME} -c 'myup'"
 	echo -e "Section "\"InputClass"\"\nIdentifier "\"system-keyboard"\"\nMatchIsKeyboard "\"on"\"\nOption "\"XkbLayout"\" "\"ch"\"\nEndSection" > /mnt/etc/X11/xorg.conf.d/00-keyboard.conf
 	arch-chroot /mnt /bin/bash -c "localectl set-x11-keymap ch nodeadkeys"
 	sed -i 's/%wheel ALL=(ALL) NOPASSWD: ALL/#%wheel ALL=(ALL) NOPASSWD: ALL/' /mnt/etc/sudoers
 	arch-chroot /mnt /bin/bash -c "chown -R ${USERNAME}:users /home/${USERNAME}"
-	arch-chroot /mnt /bin/bash -c "echo $RPASSWD | su - ${USERNAME} -c 'myup'"
+	arch_chroot "glib-compile-schemas /usr/share/glib-2.0/schemas/"
 	#Ende 
 	swapoff -a
 	umount -R /mnt
